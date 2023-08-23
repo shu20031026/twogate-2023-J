82d101f056a3987502473a2f7ee261eba51f9b88
shu20031026 <shu20031026@gmail.com>
2023-08-23 15:09:21 +0900

    fix: 不使用のconsole等の削除

--- src/pages/api/linebot.ts
+++ src/pages/api/linebot.ts
@@ -1,5 +1,4 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import * as line from '@line/bot-sdk';
 import { WebhookRequestBody } from '@line/bot-sdk';
 import { lineClient } from '~/src/linebot/utils/line';
 
@@ -11,14 +10,11 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       const body: WebhookRequestBody = req.body;
       await Promise.all(
         body.events.map(async (event) => {
-          console.log(event.type);
-
           switch (event.type) {
             case 'message': {
               console.log(event.message);
               if (event.message.type === 'text') {
                 const getText = event.message.text;
-                console.log(getText);
                 await client.replyMessage(event.replyToken, {
                   type: 'text',
                   text: getText,
@@ -34,8 +30,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       );
     }
 
-    const message = req.body.message;
-    res.status(200).json({ message: `${message}というメッセージが送信されました。` });
+    res.status(200);
   } catch (e) {
     res.status(500).json({ message: `error! ${e} ` });
   }
